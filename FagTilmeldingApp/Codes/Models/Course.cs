002c01d1459dd617334e1635b65e0929ebf0781a
Martin Monefeldt <mart69w6@elev.tec.dk>
2022-02-23 09:14:05 +0100

    4th Push

---
+++ FagTilmeldingApp/Codes/Models/Course.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FagTilmeldningApp.Codes.Models
+{
+    internal class Course
+    {
+        public int Id { get; set; }
+        public int TeacherID { get; set; }
+        public string? Coursename { get; set; }
+
+    }
+}
