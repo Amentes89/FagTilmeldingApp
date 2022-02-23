0b20f0948d9417909ec820d71adf5836565c26cc
Martin Monefeldt <mart69w6@elev.tec.dk>
2022-02-23 09:05:24 +0100

    2nd Push

--- FagTilmeldingApp/Program.cs
+++ FagTilmeldingApp/Program.cs
@@ -1,1 +1,13 @@
 ﻿// Iteration 1
+using FagTilmeldingApp.Codes;
+
+
+Semester S = new Semester();
+
+Console.Clear();
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine("-------------------------------------------------------------------------");
+Console.WriteLine(" " + S.SchoolName + ", " + S.SemesterNavn + " fag tilmeldning app.");
+Console.WriteLine("-------------------------------------------------------------------------");
+Console.ForegroundColor = ConsoleColor.White;
+Console.ReadKey();
